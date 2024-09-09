30e12d6b59cd17c1dc797fa5ecb1ef9dd62d10fa
Hamid Furkan Suluova <77333809+hfsuluova@users.noreply.github.com>
2024-09-09 11:26:47 +0100

    Update Main.m

--- Combinatorial/Main.m
+++ Combinatorial/Main.m
@@ -5,4 +5,4 @@ for tour = 1:50
     [Result_BA1C(tour).Cost , Result_BA1C(tour).NFE, Result_BA1C(tour).OptCost] = Function_BA1_NN_comparativeLocal('Eil51', 51);
 end
 
-save Hamming_Eil_51_city_size_with_Tri_comp_Local Result_BA1C
+save Eil_51_city_size_with_Tri_comp_Local Result_BA1C
